99b3d986921323f3d375946c22b5727292c91b60
Nefaratus <jschure@hotmail.com>
2015-01-08 14:02:19 +0100

    New Features
    -Teleporting to inside of the inn.
    -Added sound within the inn
    -Added Combat Sound

---
+++ Assets/Scripts/Audio/AudioZoneControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioZoneControl : MonoBehaviour {
+
+	AudioSource music;
+
+	// Use this for initialization
+	void Start () {
+		music = gameObject.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		music.enabled = true;
+		music.Play ();
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		music.Stop ();
+		music.enabled = false;
+	}
+}
---
+++ Assets/Scripts/TransferOutside.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransferOutside : MonoBehaviour {
+
+	GameObject[] transferPoint;
+	GameObject target;
+	Vector3 offset = new Vector3(0,0,-3);
+	
+	// Use this for initialization
+	void Start () {
+		transferPoint = GameObject.FindGameObjectsWithTag ("Outside");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject house in transferPoint) {
+			if(gameObject.name == house.gameObject.name)
+			{
+				target = house;
+			}
+		}
+		
+	}
+	
+	void OnTriggerEnter(Collider col)
+	{
+		col.gameObject.transform.position = target.gameObject.transform.position + offset;
+	}
+	
+	void TeleportPlayer()
+	{
+		
+	}
+}
