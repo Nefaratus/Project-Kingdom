b627e5817deea9770ae4051cd6a9b0aed81dfa27
Sietsewer <sietsewer@gmail.com>
2015-01-10 18:07:09 +0100

    Terrain

---
+++ Assets/TerrainNeighbors.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainNeighbors : MonoBehaviour {
+	public Terrain top;
+	public Terrain right;
+	public Terrain bottom;
+	public Terrain left;
+	// Use this for initialization
+	void Start () {
+		Terrain t = (Terrain)this.gameObject.GetComponent(typeof(Terrain));
+		if(t != null){
+			t.SetNeighbors(left,top,right,bottom);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
